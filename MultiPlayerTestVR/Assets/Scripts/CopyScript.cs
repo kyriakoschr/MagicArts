51abdecdd96eb6d9cb862c46cdb128132008617e
kyriakoschr <48152653+kyriakoschr@users.noreply.github.com>
2020-06-04 22:11:10 +0300

    vr w/ remy

--- MultiPlayerTestVR/Assets/Scripts/CopyScript.cs
+++ MultiPlayerTestVR/Assets/Scripts/CopyScript.cs
@@ -1,16 +1,25 @@
 ﻿using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class CopyScript : MonoBehaviourPun
 {
+    public float z = 0f; 
+    public float y = -17f; 
+    public float x = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*if (photonView.IsMine)
+        {
+            transform.position = ViveManager.Instance.avatar.transform.position;
+            Vector3 temp = new Vector3(x, y, z);
+            transform.position += temp;
+            transform.parent = ViveManager.Instance.avatar.transform;
+        }*/
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -19,7 +28,18 @@ public class CopyScript : MonoBehaviourPun
             //Transform t = new Transform();
             //ViveManager.Instance.avatar.transform;
             transform.position = ViveManager.Instance.avatar.transform.position;
-            transform.rotation = ViveManager.Instance.avatar.transform.rotation;
+            Vector3 temp = new Vector3(x, y, z);
+            transform.position += temp;
+            Transform t = this.transform;
+            t.eulerAngles = new Vector3(0, t.rotation.eulerAngles.y, 0);
+            //transform.rotation = ViveManager.Instance.avatar.transform.rotation;
         }
+    }
+
+    void LateUpdate()
+    {
+        /*Transform t = this.transform;
+        t.eulerAngles = new Vector3(0, t.rotation.eulerAngles.y, 0);*/
+
     }
 }
