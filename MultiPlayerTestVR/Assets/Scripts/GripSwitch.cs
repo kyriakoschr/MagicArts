2342f69f1b4104fa1358b66ad21f0af660efa69d
kyriakoschr <48152653+kyriakoschr@users.noreply.github.com>
2020-07-07 17:49:41 +0300

    card and placeholder for storyteller

---
+++ MultiPlayerTestVR/Assets/Scripts/GripSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GripSwitch : MonoBehaviour
+{
+    public GameObject simulator;
+    public GameObject LTouchPad;
+    public GameObject RTouchPad;
+    public GameObject MouseClick1;
+    public InteractorFacade interactor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
