1623d1421eae173dfc7dcf747d8cd6b73e97a02d
kyriakoschr <48152653+kyriakoschr@users.noreply.github.com>
2020-07-07 22:29:45 +0300

    switch of interactor according to Rig

--- MultiPlayerTestVR/Assets/Scripts/GripSwitch.cs
+++ MultiPlayerTestVR/Assets/Scripts/GripSwitch.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Tilia.Interactions.Interactables.Interactors;
 using UnityEngine;
+using Zinnia.Action;
 
 public class GripSwitch : MonoBehaviour
 {
     public GameObject simulator;
-    public GameObject LTouchPad;
-    public GameObject RTouchPad;
-    public GameObject MouseClick1;
+    public BooleanAction VRAction;
+    public BooleanAction SimAction;
     public InteractorFacade interactor;
+
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+
+    public void VRON()
+    {
+        interactor.GrabAction = VRAction;
+    }
+
+    public void SimON()
+    {
+        interactor.GrabAction = SimAction;
     }
 
     // Update is called once per frame
---
+++ MultiPlayerTestVR/Assets/Scripts/OnEnableOfSimulator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnEnableOfSimulator : MonoBehaviour
+{
+    public GripSwitch lInteractor;
+    public GripSwitch rInteractor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnEnable()
+    {
+        lInteractor.SimON();
+        rInteractor.SimON();
+    }
+
+    private void OnDisable()
+    {
+        lInteractor.VRON();
+        rInteractor.VRON();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
